379cfdf20f908b7f173c8419a5dc95e2efc2aafb
AruncapChek <arunprasath0020@gmail.com>
2026-06-03 13:45:45 +0530

    spell check correction

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,7 +2,7 @@ import { configureStore } from "@reduxjs/toolkit";
 
 import cartReducer from "../features/cart/cartSlice";
 import productReducer from "../features/products/productSlice";
-import wishlistReducer from "../features/wishlist/wishListSlice";
+import wishlistReducer from "../features/wishList/wishListSlice";
 import userReducer from "../features/user/userSlice";
 
 export const store = configureStore({
